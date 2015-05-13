a6da024a7c538d2d6ca41e4349e0e0334d28d06b
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-05-13 21:44:40 +0200

    Improve lab.api.js

--- core/api/lab.api.js
+++ core/api/lab.api.js
@@ -1,20 +1,10 @@
 (function() {
     'use strict';
 
-    if (!Date.prototype.getDeathHour || !Date.prototype.countDown) {
+    if (!Date.prototype.getDeathHour) {
         Date.prototype.getDeathHour = function (created, minutes) {
             this.setTime(created.getTime() + minutes*60000);
             return this;
-        };
-
-        Date.prototype.countDown = function (time, isDead, setExistence) {
-            var limit = time - 1;
-                if (isDead !== undefined) {
-                    isDead(limit);
-                }
-                if (setExistence !== undefined) {
-                    setExistence(limit);
-                }
         };
     }
 
