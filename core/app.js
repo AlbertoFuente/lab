98ffa7e9bb7f6ad367469cc23bcab9034597c803
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-05-16 00:58:57 +0200

    Array options for config select options inside index.html

--- core/app.js
+++ core/app.js
@@ -5,9 +5,12 @@ define(['jquery', 'lodash', 'api', 'components', 'events'], function($, _, _api,
         init: function() {
             var dashboard = document.getElementById('dashboard'),
                 configPanel = document.getElementById('configPanel'),
-                particle = _comp.elementalParticle();
-
-            console.log(configPanel);
+                // Print Select table size options
+                sizeOptions = ['600 x 800', '800 x 1200', '1200 x 1400', '1400 x 1600'],
+                tableSizeOptions = _api.detectDomElement(configPanel, 'tableSize', sizeOptions),
+                // Print number of particles options
+                particleOptions = ['Random', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100'],
+                numberParticleOptions = _api.detectDomElement(configPanel, 'numberOfParticles', particleOptions);
         }
     };
 });
