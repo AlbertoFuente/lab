010ef25ecc376a8ef83fbd33124e4e34abc1baa8
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-05-15 07:57:14 +0200

    Create app.js

---
+++ core/app.js
@@ -0,0 +1,10 @@
+define(function() {
+    'use strict';
+
+    return {
+        init: function(components, api) {
+            var particle = components.elementalParticle(api);
+            console.log(particle);
+        }
+    };
+}());
