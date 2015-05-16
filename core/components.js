819d89fb830e6b3eaca3814227069f13d0aac52e
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-05-16 17:25:29 +0200

    Improve core/components.js

--- core/components.js
+++ core/components.js
@@ -13,20 +13,37 @@ define(['jquery', 'lodash', 'api'], function($, _, _api) {
 
             return this;
         },
-        tableSize: function (size) {
+        tableConfig: function (height, width) {
+            return {
+                height: height,
+                width: width
+            };
+        },
+        tableObject: {},
+        tableSizes: function (size) {
             switch (size) {
                 case '600 x 800':
+                    this.tableObject = this.tableConfig('600', '800');
                     break;
                 case '800 x 1200':
+                    this.tableObject = this.tableConfig('800', '1200');
                     break;
                 case '1200 x 1400':
+                    this.tableObject = this.tableConfig('1200', '1400');
                     break;
                 case '1400 x 1600':
+                    this.tableObject = this.tableConfig('1400', '1600');
                     break;
             }
         },
+        numberOfParticles: function (num) {
+            if (this.tableObject !== undefined && this.tableObject.width !== undefined) {
+                this.tableObject.particles = num;
+                this.createTable(this.tableObject);
+            }
+        },
         createTable: function(obj) {
-
+            console.log(obj);
         }
     };
 });
