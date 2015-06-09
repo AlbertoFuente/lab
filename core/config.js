115ba13ab18cb05849ee878dd8a450921bb81664
AlbertoFuente <alberto.delafuente.adesis@gmail.com>
2015-06-09 20:53:27 +0200

    Improve config.js

--- core/config.js
+++ core/config.js
@@ -3,32 +3,11 @@ requirejs.config({
     paths: {
         // vendor
         'jquery': '../bower_components/jquery/dist/jquery.min',
-        'lodash': '../bower_components/lodash/lodash.min',
-        // scripts
-        'api': 'api',
-        'components': 'components',
-        'app': 'app'
-  },
-  shim: {
-        'api': {
-            exports: 'api',
-        },
-        'components': {
-            exports: 'components'
-        },
-        'jquery': {
-            exports: 'jquery'
-        },
-        'lodash': {
-            exports: 'lodash'
-        },
-        'app': {
-            exports: 'app'
-        }
+        'lodash': '../bower_components/lodash/lodash.min'
     }
 });
 
-require(['jquery', 'lodash', 'components', 'api', 'app'], function($, _, comp, api, app){
+require(['jquery', 'lodash', 'components', 'api', 'app'], function($, _, comp, api, app) {
     'use strict';
-	app.init();
+    app.init();
 });
